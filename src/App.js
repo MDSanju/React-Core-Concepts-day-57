4ca90b3ed7abed6c3e1a2e773014cf589e7d190e
MD Sanju <mdsanju387@gmail.com>
2021-12-13 19:37:20 +0600

    react core concepts day 57

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { flushSync } from 'react-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* counter */}
+      <MyComponent></MyComponent>
+
+      {/* fetch */}
+      <Users></Users>
+    </div>
+  );
+}
+
+// set state / a counter
+function MyComponent() {
+  // declare state
+  const [multiplication, setMultiplication] = useState(1);
+  // counter function
+  const myCounter = () => setMultiplication(multiplication * 2);
+  return (
+    <div>
+      <h1>Multiplication: <span style={{color: '#e95d2d'}}>{multiplication}</span></h1>
+      <button style={{backgroundColor: 'orangered', color: 'white', border: 'none', padding: '9px 17px', fontWeight: '600'}} onClick={myCounter}>Increase</button>
+    </div>
+  );
+}
+
+// fetch API call
+function Users() {
+  // set state
+  const [users, setUSers] = useState([]);
+
+  // useEffect
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUSers(data))
+  }, [])
+  return (
+    <div>
+        {
+          users.map(user => <User username={user.username} email={user.email}></User>)
+        }
+    </div>
+  );
+}
+
+// component for dynamic fetch API
+function User(props) {
+  return (
+    <div className="container">
+      <div className="userIndv">
+        <h3>Username: {props.username}</h3>
+        <p>Email: {props.email}</p>
+      </div>
     </div>
   );
 }
